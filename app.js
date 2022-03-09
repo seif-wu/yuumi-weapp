1c8ca0d788e1f88607aedd39e80eaed260eb5076
Seif Wu <seif.wuxf@gmail.com>
2022-03-09 22:32:38 +0800

    feat: 添加获取导航栏信息

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 // app.js
 App({
   onLaunch() {
-    // 展示本地存储能力
-    const logs = wx.getStorageSync('logs') || []
-    logs.unshift(Date.now())
-    wx.setStorageSync('logs', logs)
-
-    // 登录
-    wx.login({
-      success: res => {
-        // 发送 res.code 到后台换取 openId, sessionKey, unionId
-      }
-    })
+    const systemInfo = wx.getSystemInfoSync();
+    // 胶囊按钮位置信息
+    const menuButtonInfo = wx.getMenuButtonBoundingClientRect();
+    // 导航栏高度 = 状态栏到胶囊的间距（胶囊距上距离-状态栏高度） * 2 + 胶囊高度 + 状态栏高度
+    this.globalData.navBarHeight = (menuButtonInfo.top - systemInfo.statusBarHeight) * 2 + menuButtonInfo.height + systemInfo.statusBarHeight;
+    this.globalData.menuRight = systemInfo.screenWidth - menuButtonInfo.right;
+    this.globalData.menuButton = menuButtonInfo.top - systemInfo.statusBarHeight;
+    this.globalData.menuHeight = menuButtonInfo.height;
   },
+
   globalData: {
-    userInfo: null
+    // 导航栏高度
+    navBarHeight: 0,
+    // 胶囊距右方间距（方保持左、右间距一致）
+    menuRight: 0,
+    // 胶囊距底部间距（保持底部间距一致）
+    menuButton: 0,
+    // 胶囊高度（自定义内容可与胶囊高度保证一致）
+    menuHeight: 0,
+    // 当前用户
+    currentUser: null,
   }
 })
